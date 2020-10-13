374f976bfb94b07f2daf8ca9988671ad7c8bc95f
aviy <aviy.vai@gmail.com>
2020-10-13 13:36:17 +0300

    Remove non ready mojos

--- src/main/java/com/outbrain/ci/friendly/flatten/maven/plugin/VersionProvider.java
+++
@@ -1,7 +0,0 @@
-package com.outbrain.ci.friendly.flatten.maven.plugin;
-
-import org.apache.maven.plugin.MojoExecutionException;
-
-public interface VersionProvider {
-   String getVersion(String command) throws MojoExecutionException;
-}
