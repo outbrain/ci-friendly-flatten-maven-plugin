2a6c63bd17cfb933f8e9dfb8e7e030fa173c53e9
aviy <aviy.vai@gmail.com>
2020-10-21 14:16:34 +0300

    Add finals

--- src/main/java/com/outbrain/ci/friendly/flatten/maven/plugin/visitor/PomVisitor.java
+++ src/main/java/com/outbrain/ci/friendly/flatten/maven/plugin/visitor/PomVisitor.java
@@ -1,7 +1,7 @@
 package com.outbrain.ci.friendly.flatten.maven.plugin.visitor;
 
 public interface PomVisitor {
-  default String visit(final String originalPom, final String revision, String sha1, String changeList){
+  default String visit(final String originalPom, final String revision, final String sha1, final String changeList){
     String modified;
     modified = originalPom.replace("${revision}", revision);
     if (originalPom.contains("${sha1}")) {
