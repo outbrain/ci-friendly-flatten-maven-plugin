1680b97f6c3e6645dfbbab358e3ea62ffff99f40
aviy <aviy.vai@gmail.com>
2020-10-21 14:20:25 +0300

    Tiny changes in condition

--- src/main/java/com/outbrain/ci/friendly/flatten/maven/plugin/visitor/PomVisitor.java
+++ src/main/java/com/outbrain/ci/friendly/flatten/maven/plugin/visitor/PomVisitor.java
@@ -4,11 +4,11 @@ public interface PomVisitor {
   default String visit(final String originalPom, final String revision, final String sha1, final String changeList){
     String modified;
     modified = originalPom.replace("${revision}", revision);
-    if (originalPom.contains("${sha1}")) {
+    if (modified.contains("${sha1}")) {
       modified = modified.replace("${sha1}", sha1 != null ? sha1 : "");
     }
 
-    if (originalPom.contains("${changelist}")) {
+    if (modified.contains("${changelist}")) {
       modified = modified.replace("${changelist}", changeList != null ? changeList : "");
     }
 
