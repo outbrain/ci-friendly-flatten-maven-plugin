65733ce5875274378a675470129ff0716723b6e9
aviy <aviy.vai@gmail.com>
2021-10-31 17:06:00 +0200

    Remove jupiter + Add versionUtil

--- src/test/java/com/outbrain/ci/friendly/flatten/maven/plugin/visitor/VersionPrefixTest.java
+++ src/test/java/com/outbrain/ci/friendly/flatten/maven/plugin/visitor/VersionPrefixTest.java
@@ -9,14 +9,14 @@ import static org.junit.Assert.assertEquals;
 public class VersionPrefixTest {
 
   @Test
-  public void testVersionMatcher1() throws Exception {
+  public void testVersionMatcher1() {
 
     final String version = VersionUtil.getVersion("abc-dsffsd-44.0.0.0", "-");
     assertEquals("44.0.0.0", version);
   }
 
   @Test
-  public void testVersionMatcher2() throws Exception {
+  public void testVersionMatcher2() {
 
     final String version = VersionUtil.getVersion("abc-4.0.0.0.0.0.0", "-");
     assertEquals("4.0.0.0.0.0.0", version);
